86e3c194873ae27a95ffbc6803c7733df2eb6490
moisecas <62753698+moisecas@users.noreply.github.com>
2021-10-25 14:36:22 -0500

    Add files via upload

---
+++ Funciones.py
@@ -0,0 +1,12 @@
+def imprimir_mensaje():
+    print("mensaje especial")
+    print("estoy aprendiendo a usar funciones")
+    numero1 = 2
+    numero2 = 3
+    print(numero1+numero2)
+
+
+
+imprimir_mensaje()
+imprimir_mensaje()
+imprimir_mensaje() 
